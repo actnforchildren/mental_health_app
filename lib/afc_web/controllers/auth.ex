49f339c66eae2b30033ef35e3c5021dfb71f1cba
Robert Francis <robertefrancis18@gmail.com>
2018-10-15 17:24:59 +0100

    creates basic auth plug to protect routes #6 #7

---
+++ lib/afc_web/controllers/auth.ex
@@ -0,0 +1,34 @@
+defmodule AfcWeb.Auth do
+  import Plug.Conn
+  import Phoenix.Controller
+  alias Afc.{Repo, User}
+  alias AfcWeb.Router.Helpers
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && Repo.get(User, user_id)
+    assign(conn, :current_user, user)
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  def logout(conn), do: configure_session(conn, drop: true)
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Helpers.session_path(conn, :new))
+      |> halt()
+    end
+  end
+end
