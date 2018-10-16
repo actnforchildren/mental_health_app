ce14c7054f2ac9ce76973d10b010384641817a96
Robert Francis <robertefrancis18@gmail.com>
2018-10-16 11:49:09 +0100

    allows a user to record their emotions #29 #22

--- lib/afc/emotion_log.ex
+++ lib/afc/emotion_log.ex
@@ -1,11 +1,12 @@
 defmodule Afc.EmotionLog do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Afc.EctoEnum.EmotionEnum
+  alias Afc.{EctoEnum.EmotionEnum, User}
 
   schema "emotion_logs" do
     field :emotion, EmotionEnum
-    field :user_id, :id
+    field :emotion_id, :id
+    belongs_to :user, User
 
     timestamps()
   end
@@ -13,7 +14,7 @@ defmodule Afc.EmotionLog do
   @doc false
   def changeset(emotion_log, attrs) do
     emotion_log
-    |> cast(attrs, [:emotion])
-    |> validate_required([:emotion])
+    |> cast(attrs, [:emotion, :emotion_id])
+    |> validate_required([:emotion, :emotion_id])
   end
 end
