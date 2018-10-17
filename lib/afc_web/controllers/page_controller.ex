6b57a7382420f1d4c21d2e2bfb7e7065d8a4c284
Robert Francis <robertefrancis18@gmail.com>
2018-10-17 15:45:39 +0100

    #29 #34 creates single emotion page for displaying users today's log

--- lib/afc_web/controllers/page_controller.ex
+++ lib/afc_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule AfcWeb.PageController do
   use AfcWeb, :controller
+  alias Afc.Emotion
 
   def index(conn, _params) do
-    render conn, "index.html"
+    case Emotion.todays_logged_emotion(conn.assigns.current_user) do
+      nil ->
+        render conn, "index.html"
+      emotion ->
+        render conn, "single.html", emotion: emotion
+    end
   end
 end
