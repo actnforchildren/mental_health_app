83b2916dfcce9d516eb554f4acac588448289dda
Robert Francis <robertefrancis18@gmail.com>
2018-10-17 15:40:49 +0100

    #29 #34 adds function to check if user has logged emoji today

--- lib/afc/emotion/emotion.ex
+++ lib/afc/emotion/emotion.ex
@@ -1,3 +1,17 @@
 defmodule Afc.Emotion do
+  alias Afc.Repo
+  alias Afc.Emotion.EmotionLog
+  import Ecto.Query
+  use Timex
 
+  def todays_logged_emotion(user) do
+    start_of_today = Timex.today() |> Timex.to_naive_datetime()
+
+    query =
+      from e in EmotionLog,
+      where: e.user_id == ^user.id
+      and e.inserted_at > ^start_of_today
+
+    Repo.one(query)
+  end
 end
