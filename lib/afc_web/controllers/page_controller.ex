36511c161e662240c6eb1359381008a7025217b8
Robert Francis <robertefrancis18@gmail.com>
2018-10-18 11:24:22 +0100

    adds info and styles single emotion page #34

--- lib/afc_web/controllers/page_controller.ex
+++ lib/afc_web/controllers/page_controller.ex
@@ -1,13 +1,16 @@
 defmodule AfcWeb.PageController do
   use AfcWeb, :controller
-  alias Afc.Emotion
+  alias Afc.{Emotion, Repo}
 
   def index(conn, _params) do
     case Emotion.todays_emotion_log(conn.assigns.current_user) do
       nil ->
         render conn, "index.html"
-      emotion ->
-        render conn, "single.html", emotion: emotion
+      emotion_log ->
+        module_name = Emotion.get_emotion_module_name(emotion_log.emotion)
+        emotion = Repo.get(module_name, emotion_log.emotion_id)
+
+        render conn, "single.html", emotion_log: emotion_log, emotion: emotion
     end
   end
 end
