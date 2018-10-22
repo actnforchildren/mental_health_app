2a1c9224aa49aa42d90dc717664fcf8d82bd81ca
Robert Francis <robertefrancis18@gmail.com>
2018-10-22 13:01:06 +0100

    changes site gb colour depending on url #42

--- lib/afc_web/views/layout_view.ex
+++ lib/afc_web/views/layout_view.ex
@@ -1,3 +1,11 @@
 defmodule AfcWeb.LayoutView do
   use AfcWeb, :view
+
+  def background_colour_picker(conn) do
+    if String.contains?(conn.request_path, "/emotion/") do
+      "bg-white"
+    else
+      "afc-bg-pink"
+    end
+  end
 end
