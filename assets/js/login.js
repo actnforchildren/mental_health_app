fd654f8973dc13a2b4395e854c4d14b2903f6b27
Robert Francis <robertefrancis18@gmail.com>
2018-10-23 19:36:40 +0100

    creates and styles a user login page #5

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+if (document.getElementsByClassName("pin")) {
+  console.log("fdsjlfjsdfklsadjfklsdfjsdklf");
+  const pin_inputs = Array.from(document.getElementsByClassName("pin"))
+
+  pin_inputs.map((el, i) => {
+    el.onkeyup = () => {
+      if (el.value.length == el.maxLength) {
+        pin_inputs[i + 1].focus()
+      }
+    }
+  })
+}
--- lib/afc_web/views/layout_view.ex
+++ lib/afc_web/views/layout_view.ex
@@ -2,7 +2,9 @@ defmodule AfcWeb.LayoutView do
   use AfcWeb, :view
 
   def background_colour_picker(conn) do
-    if String.contains?(conn.request_path, "/emotion/") do
+    white_bg_paths = ~w(/emotion/ /sessions)
+
+    if Enum.any?(white_bg_paths, &String.contains?(conn.request_path, &1)) do
       "bg-white"
     else
       "afc-bg-pink"
