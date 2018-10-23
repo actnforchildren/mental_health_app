e545c0ebe22cbded835083ca860b4f336f43dcf8
SimonLab <simon.labondance@gmail.com>
2018-10-23 21:56:15 +0100

    replace emoji by image

--- lib/afc_web/views/component_helpers.ex
+++ lib/afc_web/views/component_helpers.ex
@@ -2,10 +2,26 @@ defmodule AfcWeb.ComponentHelpers do
   alias AfcWeb.ComponentView
   use Phoenix.HTML
 
+
   @moduledoc false
 
   def component(template, assigns \\ []) do
     ComponentView.render "#{template}.html", assigns
+  end
+
+  def src_image(image) do
+    image = if is_atom(image), do: Atom.to_string(image), else: image
+    "/images/" <> image <> ".png"
+  end
+
+  def emoji_img_tag(src, opts \\ []) do
+    case Keyword.fetch(opts, :class) do
+      {:ok, classes} ->
+        opts = Keyword.delete(opts, :class)
+        content_tag(:img, "", [class: "w4 " <> classes, src: src] ++ opts)
+      :error ->
+        content_tag(:img, "", [class: "w4 ", src: src] ++ opts)
+    end
   end
 
   def emoji_p_tag(emotion, opts \\ []) do
@@ -20,13 +36,13 @@ defmodule AfcWeb.ComponentHelpers do
     end
   end
 
-  def render_emotion_reason(emotion) do
+  def render_emotion_reason(emotion, conn) do
     positive_reasons = ["happy", "excited"]
 
     if Enum.any?(positive_reasons, &(&1 == emotion)) do
-      component("positive_emotion_reason", emotion: emotion)
+      component("positive_emotion_reason", emotion: emotion, conn: conn)
     else
-      component("negative_emotion_reason", emotion: emotion)
+      component("negative_emotion_reason", emotion: emotion, conn: conn)
     end
   end
 
