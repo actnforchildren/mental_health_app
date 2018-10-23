1dd8e3c4bdffe7a0ccd7653ee15868e99892449a
Robert Francis <robertefrancis18@gmail.com>
2018-10-23 21:24:52 +0100

    makes function create data only if user did not exist

--- priv/repo/create_dummy_user.exs
+++ priv/repo/create_dummy_user.exs
@@ -1,6 +1,5 @@
 alias Afc.{Repo, TrustedAdult, User}
 alias Afc.Emotion.{Angry, Else, EmotionLog, Excited, Happy, Sad, Unsure, Worried}
-alias Ecto.Changeset
 
 emotions = [
   {Angry, "angry"},
@@ -20,27 +19,25 @@ adult =
       adult
   end
 
-user =
-  case Repo.get_by(User, username: "test_user") do
-    nil ->
-      Repo.insert!(%User{username: "test_user", pin: 1234, trusted_adult_id: adult.id})
-    user ->
-      user
-  end
+case Repo.get_by(User, username: System.get_env("DUMMY_USER")) do
+  nil ->
+    user = Repo.insert!(%User{username: System.get_env("DUMMY_USER"), pin: 1234, trusted_adult_id: adult.id})
 
-today = Timex.today() |> Timex.to_naive_datetime()
-rest_of_week = Enum.map(1..6, &Timex.shift(today, days: -&1))
-week = [today] ++ rest_of_week
+    today = Timex.today() |> Timex.to_naive_datetime()
+    rest_of_week = Enum.map(1..6, &Timex.shift(today, days: -&1))
 
-  Enum.map(week, fn(day) ->
-    {emotion_module, emotion_str} = Enum.random(emotions)
-    module_struct = struct(emotion_module)
-    changeset = emotion_module.changeset(module_struct, %{reason: "test reason"})
-    emotion = Repo.insert!(changeset)
-    Repo.insert!(%EmotionLog{
-      emotion: emotion_str,
-      emotion_id: emotion.id,
-      inserted_at: day, updated_at: day,
-      user_id: user.id
-    })
-  end)
+      Enum.map(rest_of_week, fn(day) ->
+        {emotion_module, emotion_str} = Enum.random(emotions)
+        module_struct = struct(emotion_module)
+        changeset = emotion_module.changeset(module_struct, %{reason: "test reason"})
+        emotion = Repo.insert!(changeset)
+        Repo.insert!(%EmotionLog{
+          emotion: emotion_str,
+          emotion_id: emotion.id,
+          inserted_at: day, updated_at: day,
+          user_id: user.id
+        })
+      end)
+  user ->
+    user
+end
