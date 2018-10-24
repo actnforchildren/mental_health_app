cf0e4b7484fe22c8346150f6c98967891c55ec69
Robert Francis <robertefrancis18@gmail.com>
2018-10-24 15:49:19 +0100

    removes unused functions. replaced by emoji_img_tag

--- lib/afc_web/views/component_helpers.ex
+++ lib/afc_web/views/component_helpers.ex
@@ -24,18 +24,6 @@ defmodule AfcWeb.ComponentHelpers do
     end
   end
 
-  def emoji_p_tag(emotion, opts \\ []) do
-    emoji = string_to_emoji(emotion)
-
-    case Keyword.fetch(opts, :class) do
-      {:ok, classes} ->
-        opts = Keyword.delete(opts, :class)
-        content_tag(:p, emoji, [class: "emoji-font-size " <> classes] ++ opts)
-      :error ->
-        content_tag(:p, emoji, [class: "emoji-font-size"] ++ opts)
-    end
-  end
-
   def render_emotion_reason(emotion, conn) do
     positive_reasons = ["happy", "excited"]
 
@@ -43,22 +31,6 @@ defmodule AfcWeb.ComponentHelpers do
       component("positive_emotion_reason", emotion: emotion, conn: conn)
     else
       component("negative_emotion_reason", emotion: emotion, conn: conn)
-    end
-  end
-
-  defp string_to_emoji(emotion) do
-    emotion = if is_atom(emotion), do: Atom.to_string(emotion), else: emotion
-
-    emotion
-    |> String.downcase()
-    |> case do
-      "happy" -> "😆"
-      "excited" -> "🤩"
-      "angry" -> "😡"
-      "sad" -> "😭"
-      "worried" -> "😬"
-      "unsure" -> "🤔"
-      "else" -> "😶"
     end
   end
 end
