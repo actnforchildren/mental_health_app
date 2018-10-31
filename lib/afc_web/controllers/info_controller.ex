83b34785eda334545398257378a690da67375f27
Robert Francis <robertefrancis18@gmail.com>
2018-10-31 20:24:16 +0000

    adds content to the more info pages #19 #80

--- lib/afc_web/controllers/info_controller.ex
+++ lib/afc_web/controllers/info_controller.ex
@@ -2,7 +2,7 @@ defmodule AfcWeb.InfoController do
   use AfcWeb, :controller
 
   def show(conn, %{"id" => page}) do
-    static_pages = ~w(what_is_mental_wellbeing mental_wellbeing_vs_mental_health what_is_change the_impact_of_change)
+    static_pages = ~w(what_is_mental_wellbeing what_affects_mental_wellbeing what_is_change coping_with_change)
     case Enum.any?(static_pages, &(&1 == page)) do
       true ->
         render conn, "#{page}.html"
