4dfb92c3b448edb3ed2655175726123f3300686c
Robert Francis <robertefrancis18@gmail.com>
2018-10-31 17:03:48 +0000

    creates more info page #19

--- lib/afc_web/views/layout_view.ex
+++ lib/afc_web/views/layout_view.ex
@@ -2,7 +2,7 @@ defmodule AfcWeb.LayoutView do
   use AfcWeb, :view
 
   def background_colour_picker(conn) do
-    white_bg_paths = ~w(/emotion/ /sessions /help)
+    white_bg_paths = ~w(/emotion/ /sessions /help /info)
 
     if Enum.any?(white_bg_paths, &String.contains?(conn.request_path, &1)) do
       "bg-white"
